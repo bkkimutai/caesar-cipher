3de253fd2494c56701517f343b9901dc1b72979d
bkkimutai <kbmsama@gmail.com>
2023-08-21 08:38:10 +0300

    unit testing

---
+++ src/test/java/ke/co/bkkimutai/CaesarTest.java
@@ -0,0 +1,38 @@
+package ke.co.bkkimutai;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CaesarTest {
+
+    @Test
+    public void testEncryptMessage() {
+        Caesar caesar = new Caesar("Hello", "encrypt", 3);
+        String encryptedMessage = caesar.encryptMessage();
+        assertEquals("Khoor", encryptedMessage);
+    }
+
+    @Test
+    public void testDecryptMessage() {
+        Caesar caesar = new Caesar("Khoor", "decrypt", 3);
+        String decryptedMessage = caesar.decryptMessage();
+        assertEquals("Hello", decryptedMessage);
+    }
+
+    @Test
+    public void testEncryptMessageWithSpaces() {
+        Caesar caesar = new Caesar("Hello World", "encrypt", 3);
+        String encryptedMessage = caesar.encryptMessage();
+        assertEquals("Khoor Zruog", encryptedMessage);
+    }
+
+    @Test
+    public void testDecryptMessageWithSpaces() {
+        Caesar caesar = new Caesar("Khoor Zruog", "decrypt", 3);
+        String decryptedMessage = caesar.decryptMessage();
+        assertEquals("Hello World", decryptedMessage);
+    }
+}
