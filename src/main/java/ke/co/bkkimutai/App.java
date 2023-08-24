8583342eb2aba0f148e97831eb2aa145138e712e
bkkimutai <kbmsama@gmail.com>
2023-08-24 07:27:14 +0300

    optimizing while loop

--- src/main/java/ke/co/bkkimutai/App.java
+++ src/main/java/ke/co/bkkimutai/App.java
@@ -7,26 +7,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean programRunning = true;
+        while (programRunning) {
+            System.out.print("Welcome Caesar EncryptDecrypt, please select your option (encrypt, decrypt or Quit): ");
+            String process = scanner.next(); // Read the process (encrypt/decrypt) as a string
 
-        System.out.print("Alright, please enter process (encrypt/decrypt): ");
-        String process = scanner.next(); // Read the process (encrypt/decrypt) as a string
+            if (process.toLowerCase().startsWith("e")) {
 
-        System.out.print("Please enter the message: ");
-        String message = scanner.next(); // Read the message as a string
+                System.out.print("Please enter the message: ");
+                String message = scanner.next(); // Read the message as a string
 
-        System.out.print("Please enter the key: ");
-        int key = scanner.nextInt(); // Read the key as an integer
+                System.out.print("Please enter the key: ");
+                int key = scanner.nextInt(); // Read the key as an integer
 
-        Caesar caesar = new Caesar(message, process, key);
+                Caesar caesar = new Caesar(message, process, key);
+                String encryptedMessage = caesar.encryptMessage();
+                System.out.println("Encrypted Message: " + encryptedMessage);
+            }else if(process.toLowerCase().startsWith("d")) {
+                System.out.print("Please enter the message: ");
+                String message = scanner.next(); // Read the message as a string
 
-        if (process.equals("encrypt")) {
-            String encryptedMessage = caesar.encryptMessage();
-            System.out.println("Encrypted Message: " + encryptedMessage);
-        } else if (process.equals("decrypt")) {
-            String decryptedMessage = caesar.decryptMessage();
-            System.out.println("Decrypted Message: " + decryptedMessage);
-        } else {
-            System.out.println("Invalid process. Please enter 'encrypt' or 'decrypt'.");
+                System.out.print("Please enter the key: ");
+                int key = scanner.nextInt(); // Read the key as an integer
+
+                Caesar caesar = new Caesar(message, process, key);
+                String decryptedMessage = caesar.decryptMessage();
+                System.out.println("Decrypted Message: " + decryptedMessage);
+            }else if(process.toLowerCase().startsWith("q")){
+                programRunning = false;
+                System.out.print("Goodbye!");
+            } else {
+                System.out.println("Invalid process. Please enter 'encrypt', 'decrypt' or Exit.");
+            }
         }
     }
 }
